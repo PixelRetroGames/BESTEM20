988c776eabd1a3b725b54306803c49124b370c17
PinkiePie1189 <mircea_costin84@yahoo.com>
2020-11-08 00:49:11 +0200

    Added end turn button functionality

---
+++ Assets/EndTurn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTurn : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void OnClick() {
+        // for two players break glass
+        transform.parent.parent.GetComponentInChildren<Player>().GetComponentInChildren<Mana>().AddMana();
+    }
+}
