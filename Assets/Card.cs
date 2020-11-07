3e498f61a1e2658c979199288273fbef4c99feef
PixelRetroGames <pixel.retro.games@gmail.com>
2020-11-07 15:26:31 +0200

    Cards

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -7,6 +7,7 @@ public class Card : MonoBehaviour
     public string file_path;
     public int id;
     public int mana, attack, hp;
+    public string image, description;
 
 
     // Start is called before the first frame update
@@ -19,27 +20,40 @@ public class Card : MonoBehaviour
         {
             string inp_ln = inp_stm.ReadLine();
             string[] parts = inp_ln.Split('=');
-            print(parts[0]);
-            print(parts[1]);
 
-            string var = parts[0];
-            var.TrimEnd();
-            print(var);
+            string var_name = parts[0];
+            var_name = var_name.TrimEnd(' ');
             bool printed = false;
             for (int i = 0; i < var_strings.Length && !printed; i++)
             {
-                if (var.Equals(var_strings))
+                if (var_name.Equals(var_strings[i]))
                 {
                     string value = parts[1];
-                    print(var + " := " + value);
+                    print(var_name + " := " + value);
                     printed = true;
+                    if (var_name.Equals("name")) {
+                        name = value;
+                    } else if (var_name.Equals("image")) {
+                        image = value;
+                    } else if (var_name.Equals("description")) {
+                        description = value;
+                    }
                 }
             }
 
             if (!printed)
             {
                 int value = Int32.Parse(parts[1]);
-                print(var + " := " + value);
+                print(var_name + " := " + value);
+                if (var_name.Equals("id")) {
+                    id = value;
+                } else if (var_name.Equals("mana")){
+                    mana = value;
+                } else if (var_name.Equals("hp")) {
+                    hp = value;
+                } else if (var_name.Equals("attack")) {
+                    attack = value;
+                }
             }
         }
 
